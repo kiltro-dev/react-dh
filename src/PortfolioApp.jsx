33ba01d1e0dad26f7f0a3cc761b7d307445a3dc6
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2024-05-03 11:22:56 -0400

    cambios

--- src/PortfolioApp.jsx
+++ src/PortfolioApp.jsx
@@ -1,5 +1,11 @@
+import './Portfolio.css'
+
 export const PortfolioApp = () => {
   return (
-    <div>PortfolioApp</div>
+    <div>
+      <h1 className="rojo">Esta es mi aplicación de Portfolio</h1>
+      <h4>Este es mi primer componente!</h4>
+      <p>Hola mundo desde React</p>
+    </div>
   )
 }
